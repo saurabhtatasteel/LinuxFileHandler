93a463e9d02506b4854f9d854e6cc3fa09290515
saurabhtatasteel <saurabh_tatasteel@yahoo.com>
2026-05-24 20:38:18 +0530

    Added comments to the interface

--- LinuxFileHandler/BLL/Interfaces/IUploadFileProcessor.cs
+++ LinuxFileHandler/BLL/Interfaces/IUploadFileProcessor.cs
@@ -2,6 +2,9 @@
 
 namespace LinuxFileHandler.BLL.Interfaces
 {
+	/// <summary>
+	/// Interface for UploadFileProcessor
+	/// </summary>
 	public interface IUploadFileProcessor
 	{
 		Task<Tuple<MemoryStream,FileProcessResult>> ProcessFileAsync(IFormFile file, string uploadsPath, string processedPath, Guid uploadId, IWebHostEnvironment environment);
